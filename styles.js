cf0cb8c2a9887146ad6e6ac1df3eb001d5dc5c3b
ZainRashid19 <92735725+ZainRashid19@users.noreply.github.com>
2022-06-13 01:28:03 -0400

    Add files via upload

--- styles.js
+++ styles.js
@@ -1,33 +1,57 @@
-var util = {
-  mobileMenu() {
-    $("#nav").toggleClass("nav-visible");
-  },
-  windowResize() {
-    if ($(window).width() > 800) {
-      $("#nav").removeClass("nav-visible");
-    }
-  },
-  scrollEvent() {
-    var scrollPosition = $(document).scrollTop();
+/*!
+* Start Bootstrap - Freelancer v7.0.5 (https://startbootstrap.com/theme/freelancer)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
+*/
+//
+// Scripts
+//
 
-    $.each(util.scrollMenuIds, function (i) {
-      var link = util.scrollMenuIds[i],
-        container = $(link).attr("href"),
-        containerOffset = $(container).offset().top,
-        containerHeight = $(container).outerHeight(),
-        containerBottom = containerOffset + containerHeight;
+window.addEventListener('DOMContentLoaded', event => {
 
-      if (
-        scrollPosition < containerBottom - 20 &&
-        scrollPosition >= containerOffset - 20
-      ) {
-        $(link).addClass("active");
-      } else {
-        $(link).removeClass("active");
-      }
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 72,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
     });
-  }
-};
+
+});
 
 function showForm()
   {
@@ -40,17 +64,3 @@ function showForm()
     document.getElementById("contactForm").style.display = "block";
     }
   };
-
-
-
-  function cheeseballs()
-    {
-      if (document.getElementById("kayflock").style.display == "block")
-      {
-        document.getElementById("kayflock").style.display = "none";
-      }
-      else
-      {
-      document.getElementById("kayflock").style.display = "block";
-      }
-    };
